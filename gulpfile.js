0ed997a35e466b46a22db4b5762a6af7fa5d67a6
Brad Cerasani <bradcerasani@gmail.com>
2015-04-17 19:22:28 -0500

    Working example

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,15 @@
 'use strict';
 
 var gulp = require('gulp');
+
+// Gulp 4.0 recipe
 var HubRegistry = require('gulp-hub');
-
-// load some files into the registry
 var hub = new HubRegistry(['tasks/*.js']);
-
-// tell gulp to use the tasks just loaded
 gulp.registry(hub);
+
+// Gulp 3.X recipe
+// var requireDir = require('require-dir');
+// var dir = requireDir('./tasks');
+
+// Contents of default.js task
+gulp.task('default', gulp.series('clean', 'scripts'));
--- tasks/build.js
+++
@@ -1,6 +0,0 @@
-var gulp = require('gulp');
-
-gulp.task('build', gulp.series(
-  clean,
-  gulp.parallel(scripts)
-));
