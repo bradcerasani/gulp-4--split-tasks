730148786e530fec60c19c23c184f95edae24bf3
Brad Cerasani <bradcerasani@gmail.com>
2015-04-17 19:32:21 -0500

    Remove Gulp 3.0 example, add comments for default task

--- gulpfile.js
+++ gulpfile.js
@@ -7,9 +7,6 @@ var HubRegistry = require('gulp-hub');
 var hub = new HubRegistry(['tasks/*.js']);
 gulp.registry(hub);
 
-// Gulp 3.X recipe
-// var requireDir = require('require-dir');
-// var dir = requireDir('./tasks');
+// Contents of default.js task - delete the default.js task file, uncomment the line below, and the default gulp task should work
 
-// Contents of default.js task
-gulp.task('default', gulp.series('clean', 'scripts'));
+// gulp.task('default', gulp.series('clean', 'scripts'));
